33d983359f06e773bda82a6e95657af027ec7057
Damian Chavez <damian.chavez@enerminds.com>
2019-03-31 18:30:12 -0300

    COMMIT PARA LA INTEGRACION FINAL

--- OSDEPYM/src/main/java/com/osdepym/controller/HelloWorldController.java
+++ OSDEPYM/src/main/java/com/osdepym/controller/HelloWorldController.java
@@ -2,7 +2,10 @@ package com.osdepym.controller;
 
 import java.util.List;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,54 +24,45 @@ import com.osdepym.service.TestService;
 
 
 @Controller
+@Scope("session")
 public class HelloWorldController {
 	
-	protected ClassPathXmlApplicationContext context;
+	private TestService service = ConfigurationEnviroment.getInstance().getContext().getBean(TestService.class);
+	private static final Logger logger = Logger.getLogger(HelloWorldController.class);
 	
-	private TestService service;
-
 	@RequestMapping("/hello")
 	public ModelAndView welcomeMessage(@RequestParam(value = "name", required = false) String name) {		
-		// Name of your jsp file as parameter
 		ModelAndView view = new ModelAndView("hello");
 		view.addObject("name", name);
 		return view;
 	}
 	
 	@RequestMapping("/persona")
-	public ModelAndView welcomePersona(@RequestParam(value = "name", required = false) String name) {
-		
-		service = ConfigurationEnviroment.getInstance().getContext().getBean(TestService.class);
+	public ModelAndView welcomePersona(@RequestParam(value = "name", required = false) String name, HttpServletRequest request) {
+		request.getSession().setAttribute("VAS","vas");
 		List<PersonaDTO> personasDTO = service.getAllPersonas();
-		
-		// Name of your jsp file as parameter
 		ModelAndView view = new ModelAndView("persona");
 		view.addObject("personas", personasDTO);
 		return view;
 	}
 	
 	@RequestMapping(value = "/persona/{id}", method = RequestMethod.GET)
-	public String showUser(@PathVariable("id") int id, Model model) {
-
-		service = ConfigurationEnviroment.getInstance().getContext().getBean(TestService.class);
+	public String showUser(@PathVariable("id") int id, Model model, HttpServletRequest request) {
+		String attInSession = (String) request.getSession().getAttribute("VAS");
+		logger.info("Obtuve la variable en session: " + attInSession);
+		logger.debug("Obtuve la variable en session: " + attInSession);
+		logger.error("Obtuve la variable en session: " + attInSession);
 		PersonaDTO personaDTO = service.getPersonaById(id);
-		
 		if (personaDTO == null) {
 			model.addAttribute("css", "danger");
 			model.addAttribute("msg", "User not found");
 		}
-		
 		model.addAttribute("persona", personaDTO);
-
 		return "show";
-
 	}
 	
-	// show add user form
 	@RequestMapping(value = "/persona/{id}/update", method = RequestMethod.GET)
 	public String showAddPersonForm(@PathVariable("id") Integer id,Model model) {
-
-		service = ConfigurationEnviroment.getInstance().getContext().getBean(TestService.class);
 		PersonaDTO personaDTO = service.getPersonaById(id);
 
 		model.addAttribute("personaForm", personaDTO);
@@ -80,45 +74,30 @@ public class HelloWorldController {
 	
 	@RequestMapping(value = "/persona/{id}/delete", method = RequestMethod.GET)
 	public String deletePersona(@PathVariable("id") Integer id, Model model) {
-
-		service = ConfigurationEnviroment.getInstance().getContext().getBean(TestService.class);
 		service.deletePersona(id);
-
 		return "redirect:/persona?name=Sally";
 	}
 	
-	// show add user form
 	@RequestMapping(value = "/persona/add", method = RequestMethod.GET)
 	public String showAddPersonForm(Model model) {
-
-		service = ConfigurationEnviroment.getInstance().getContext().getBean(TestService.class);
-
 		model.addAttribute("personaForm", new PersonaDTO());
 		model.addAttribute("cursoList", service.getAllCursosName());
 		model.addAttribute("childrensList", service.getAllHijos());
 
 		return "personaform";
-
 	}
 	
-	// save or update user
 	@RequestMapping(value = "/personaSave", method = RequestMethod.POST)
-	public String saveOrUpdateUser(@ModelAttribute("userForm") @Validated PersonaDTO user,
-			BindingResult result, Model model, final RedirectAttributes redirectAttributes) {
-
+	public String saveOrUpdateUser(@ModelAttribute("userForm") @Validated PersonaDTO user, BindingResult result, Model model, final RedirectAttributes redirectAttributes) {
 		if (result.hasErrors()) {
 			return "personaform";
 		} else {
-
 			redirectAttributes.addFlashAttribute("css", "success");
-			
 			if(user.isNew()){
 				redirectAttributes.addFlashAttribute("msg", "User added successfully!");
 			}else{
 				redirectAttributes.addFlashAttribute("msg", "User updated successfully!");
 			}
-			
-			service = ConfigurationEnviroment.getInstance().getContext().getBean(TestService.class);
 			
 			if(user.isNew()) {
 				service.savePersona(user);
@@ -128,12 +107,7 @@ public class HelloWorldController {
 			
 			// POST/REDIRECT/GET
 			return "redirect:/persona/" + user.getNroCliente();
-
-			// POST/FORWARD/GET
-			// return "user/list";
-
 		}
 
 	}
-		
 }
