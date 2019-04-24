b7c3d9a05aba9c932184d7bda6ae1779e73dcfd2
mkj155 <43851957+mkj155@users.noreply.github.com>
2019-04-24 00:09:08 -0300

    cambio esquema

--- OSDEPYM/src/main/java/com/osdepym/hibernate/entity/Contacto.java
+++ OSDEPYM/src/main/java/com/osdepym/hibernate/entity/Contacto.java
@@ -12,7 +12,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name="CONTACTOS")
+@Table(name="CONTACTOS", schema = "tramites_online")
 public class Contacto {
 
 	@Id
