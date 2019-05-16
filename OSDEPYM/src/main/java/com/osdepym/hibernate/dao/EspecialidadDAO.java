7b9cb87a74b2797b3b87572cc7c22d87c2831bd8
Mauro Cartagironi <cartagironi.mauro@gmail.com>
2019-05-15 22:54:50 -0300

    Librerias

--- OSDEPYM/src/main/java/com/osdepym/hibernate/dao/EspecialidadDAO.java
+++
@@ -1,7 +0,0 @@
-package com.osdepym.hibernate.dao;
-
-import com.osdepym.hibernate.entity.Especialidad;
-
-public interface EspecialidadDAO extends Dao<Especialidad>{
-
-}
--- OSDEPYM/src/main/java/com/osdepym/hibernate/dao/PrestacionDAO.java
+++
@@ -1,7 +0,0 @@
-package com.osdepym.hibernate.dao;
-
-import com.osdepym.hibernate.entity.Prestacion;
-
-public interface PrestacionDAO extends Dao<Prestacion>{
-
-}
