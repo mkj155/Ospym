49394a6071e47140df8760e2e7163a5a058ecabf
mkj155 <43851957+mkj155@users.noreply.github.com>
2019-05-21 11:40:48 -0300

    nullpointer fix

--- OSDEPYM/src/main/java/com/osdepym/service/ContactoServiceImpl.java
+++ OSDEPYM/src/main/java/com/osdepym/service/ContactoServiceImpl.java
@@ -30,262 +30,264 @@ import com.osdepym.util.MailingUtil;
 import com.osdepym.util.SessionUtil;
 
 @Service("ContactService")
-public class ContactoServiceImpl implements ContactoService{
+public class ContactoServiceImpl implements ContactoService {
 
-@Autowired
-private SessionFactory sessionFactory;
+	@Autowired
+	private SessionFactory sessionFactory;
 
-public SessionFactory getSessionFactory() {
-return sessionFactory;
-}
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
 
-public void setSessionFactory(SessionFactory sessionFactory) {
-this.sessionFactory = sessionFactory;
-}
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 
-@Autowired
-private MotivoCategoriaDAO motivoCategoriaDAO ;
+	@Autowired
+	private MotivoCategoriaDAO motivoCategoriaDAO;
 
-public MotivoCategoriaDAO getMotivoCategoriaDAO() {
-return motivoCategoriaDAO;
-}
+	public MotivoCategoriaDAO getMotivoCategoriaDAO() {
+		return motivoCategoriaDAO;
+	}
 
-public void setMotivoCategoriaDAO(MotivoCategoriaDAO motivoCategoriaDAO) {
-this.motivoCategoriaDAO = motivoCategoriaDAO;
-}
+	public void setMotivoCategoriaDAO(MotivoCategoriaDAO motivoCategoriaDAO) {
+		this.motivoCategoriaDAO = motivoCategoriaDAO;
+	}
 
-@Autowired
-private MotivoDAO motivoDAO ;
+	@Autowired
+	private MotivoDAO motivoDAO;
 
-public MotivoDAO getMotivoDAO() {
-return motivoDAO;
-}
+	public MotivoDAO getMotivoDAO() {
+		return motivoDAO;
+	}
 
-public void setMotivoDAO(MotivoDAO motivoDAO) {
-this.motivoDAO = motivoDAO;
-}
+	public void setMotivoDAO(MotivoDAO motivoDAO) {
+		this.motivoDAO = motivoDAO;
+	}
 
-@Transactional
-public List<MotivoDTO> getAllMotivos() throws CustomException {
-List<MotivoDTO> motivosDTO = new ArrayList<MotivoDTO>();
-Session session = null;
-Transaction tx = null;
-try {
-session = this.sessionFactory.getCurrentSession();
-tx = session.beginTransaction();
-List<Motivo> motivos = motivoDAO.getAll();
-if (motivos != null) {
-for (Motivo motivo : motivos) {
-motivosDTO.add(entityToDTO(motivo));
-}
-}
-tx.commit();
-session.close();
-} catch (CustomException e) {
-SessionUtil.rollbackTransaction(session, tx);
-throw e;
-} catch (Exception e) {
-SessionUtil.rollbackTransaction(session, tx);
-e.printStackTrace();
-throw new CustomException(e.getMessage(), ErrorMessages.LOAD_CONTACT_ERROR);
-}
-return motivosDTO;
-}
+	@Transactional
+	public List<MotivoDTO> getAllMotivos() throws CustomException {
+		List<MotivoDTO> motivosDTO = new ArrayList<MotivoDTO>();
+		Session session = null;
+		Transaction tx = null;
+		try {
+			session = this.sessionFactory.getCurrentSession();
+			tx = session.beginTransaction();
+			List<Motivo> motivos = motivoDAO.getAll();
+			if (motivos != null) {
+				for (Motivo motivo : motivos) {
+					motivosDTO.add(entityToDTO(motivo));
+				}
+			}
+			tx.commit();
+			session.close();
+		} catch (CustomException e) {
+			SessionUtil.rollbackTransaction(session, tx);
+			throw e;
+		} catch (Exception e) {
+			SessionUtil.rollbackTransaction(session, tx);
+			e.printStackTrace();
+			throw new CustomException(e.getMessage(), ErrorMessages.LOAD_CONTACT_ERROR);
+		}
+		return motivosDTO;
+	}
 
+	private MotivoDTO entityToDTO(Motivo motivo) {
+		MotivoDTO motivoDTO = new MotivoDTO();
+		BeanUtils.copyProperties(motivo, motivoDTO);
+		return motivoDTO;
+	}
 
-private MotivoDTO entityToDTO(Motivo motivo) {
-MotivoDTO motivoDTO = new MotivoDTO();
-BeanUtils.copyProperties(motivo, motivoDTO);
-return motivoDTO;
-}
+	@Transactional
+	private String getSecuence() throws CustomException {
+		String secuence = "";
+		try {
+			Integer result = motivoCategoriaDAO.getSecuence();
+			secuence = Integer.toString(result);
+			String year = String.format("%04d", LocalDate.now().getYear());
+			String month = String.format("%02d", LocalDate.now().getMonthValue());
+			String day = String.format("%02d", LocalDate.now().getDayOfMonth());
+			secuence = year + month + day + secuence;
+		} catch (CustomException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
+		}
+		return secuence;
+	}
 
-@Transactional
-private String getSecuence() throws CustomException {
-String secuence = "";
-try {
-Integer result = motivoCategoriaDAO.getSecuence();
-secuence = Integer.toString(result);
-String year = String.format("%04d", LocalDate.now().getYear());
-String month = String.format("%02d", LocalDate.now().getMonthValue());
-String day = String.format("%02d", LocalDate.now().getDayOfMonth());
-secuence = year + month + day + secuence;
-} catch (CustomException e) {
-throw e;
-} catch (Exception e) {
-throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
-}
-return secuence;
-}
+	@Transactional
+	private void saveContacto(Contacto contacto) throws CustomException {
+		try {
+			motivoCategoriaDAO.saveContacto(contacto);
+		} catch (CustomException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
+		}
+	}
 
-@Transactional
-private void saveContacto(Contacto contacto) throws CustomException{
-try {
-motivoCategoriaDAO.saveContacto(contacto);
-} catch (CustomException e) {
-throw e;
-} catch (Exception e) {
-throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
-}
-}
+	@Transactional
+	public List<CategoriaDTO> getCategoriasByMotivoId(int idMotivo) throws CustomException {
+		List<CategoriaDTO> categoriasDTO = new ArrayList<CategoriaDTO>();
+		Session session = null;
+		Transaction tx = null;
+		try {
+			session = this.sessionFactory.getCurrentSession();
+			tx = session.beginTransaction();
+			List<Categoria> categorias = motivoCategoriaDAO.getCategoriasByMotivoId(idMotivo);
+			if (categorias != null) {
+				for (Categoria categoria : categorias) {
+					categoriasDTO.add(entityToDTO(categoria));
+				}
+			}
+			tx.commit();
+			session.close();
+		} catch (CustomException e) {
+			SessionUtil.rollbackTransaction(session, tx);
+			throw e;
+		} catch (Exception e) {
+			SessionUtil.rollbackTransaction(session, tx);
+			e.printStackTrace();
+			throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
+		}
+		return categoriasDTO;
+	}
 
-@Transactional
-public List<CategoriaDTO> getCategoriasByMotivoId(int idMotivo) throws CustomException{
-List<CategoriaDTO> categoriasDTO = new ArrayList<CategoriaDTO>();
-Session session = null;
-Transaction tx = null;
-try {
-session = this.sessionFactory.getCurrentSession();
-tx = session.beginTransaction();
-List<Categoria> categorias = motivoCategoriaDAO.getCategoriasByMotivoId(idMotivo);
-if (categorias != null) {
-for (Categoria categoria : categorias) {
-categoriasDTO.add(entityToDTO(categoria));
-}
-}
-tx.commit();
-session.close();
-} catch (CustomException e) {
-SessionUtil.rollbackTransaction(session, tx);
-throw e;
-} catch (Exception e) {
-SessionUtil.rollbackTransaction(session, tx);
-e.printStackTrace();
-throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
-}
-return categoriasDTO;
-}
+	private CategoriaDTO entityToDTO(Categoria categoria) {
+		CategoriaDTO categoriaDTO = new CategoriaDTO();
+		BeanUtils.copyProperties(categoria, categoriaDTO);
+		return categoriaDTO;
+	}
 
-private CategoriaDTO entityToDTO(Categoria categoria) {
-CategoriaDTO categoriaDTO = new CategoriaDTO();
-BeanUtils.copyProperties(categoria, categoriaDTO);
-return categoriaDTO;	
-} 
+	public String procesarContacto(ContactoForm contactoForm) throws CustomException {
+		String nroTramite = null;
+		Session session = null;
+		Transaction tx = null;
+		try {
+			session = this.sessionFactory.getCurrentSession();
+			tx = session.beginTransaction();
 
-public String procesarContacto(ContactoForm contactoForm) throws CustomException {
-String nroTramite = null;
-Session session = null;
-Transaction tx = null;
-try {
-session = this.sessionFactory.getCurrentSession();
-tx = session.beginTransaction();
+			String correo = getMailByIds(contactoForm.getIdMotivo(), contactoForm.getIdCategoria());
+			String motivo = getMotivoById(contactoForm.getIdMotivo());
+			String categoria = null;
+			if (contactoForm.getIdCategoria() != null && !contactoForm.getIdCategoria().equals(""))
+				categoria = getCategoriaById(contactoForm.getIdCategoria());
+			String message = getMessage(contactoForm, motivo, categoria);
+			nroTramite = getSecuence();
+			String subject = getSubject(contactoForm, nroTramite);
 
-String correo = getMailByIds(contactoForm.getIdMotivo(), contactoForm.getIdCategoria());
-String motivo = getMotivoById(contactoForm.getIdMotivo());
-String categoria = null;
-if(contactoForm.getIdCategoria() != null && !contactoForm.getIdCategoria().equals(""))
-categoria = getCategoriaById(contactoForm.getIdCategoria());
-String message = getMessage(contactoForm, motivo, categoria);
-nroTramite = getSecuence();
-String subject = getSubject(contactoForm, nroTramite);
+			MailingUtil mailing = new MailingUtil();
+			mailing.sendMailTLS(correo, subject, message);
 
-MailingUtil mailing = new MailingUtil();
-mailing.sendMailTLS(correo, subject, message);
+			Contacto contacto = crearContacto(contactoForm, contactoForm.getIdMotivo(), contactoForm.getIdCategoria(),
+					correo, nroTramite, "Contacto");
+			saveContacto(contacto);
 
-Contacto contacto = crearContacto(contactoForm, contactoForm.getIdMotivo(), contactoForm.getIdCategoria(), correo, nroTramite, "Contacto");
-saveContacto(contacto);
+			tx.commit();
+			session.close();
+		} catch (CustomException e) {
+			SessionUtil.rollbackTransaction(session, tx);
+			throw e;
+		} catch (Exception e) {
+			SessionUtil.rollbackTransaction(session, tx);
+			e.printStackTrace();
+			throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
+		}
+		return nroTramite;
+	}
 
-tx.commit();
-session.close();
-} catch (CustomException e) {
-SessionUtil.rollbackTransaction(session, tx);
-throw e;
-} catch (Exception e) {
-SessionUtil.rollbackTransaction(session, tx);
-e.printStackTrace();
-throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
-}
-return nroTramite;
-}
+	@Transactional
+	private String getMotivoById(String idMotivo) throws CustomException {
+		String motivo = null;
+		try {
+			motivo = motivoCategoriaDAO.getMotivoById(idMotivo);
+		} catch (CustomException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
+		}
+		return motivo;
+	}
 
-@Transactional 
-private String getMotivoById(String idMotivo) throws CustomException {
-String motivo = null;
-try {
-motivo = motivoCategoriaDAO.getMotivoById(idMotivo);
-} catch (CustomException e) {
-throw e;
-} catch (Exception e) {
-throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
-}
-return motivo;
-}
+	@Transactional
+	private String getCategoriaById(String idCategoria) throws CustomException {
+		String categoria = null;
+		try {
+			categoria = motivoCategoriaDAO.getCategoriaById(idCategoria);
+		} catch (CustomException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
+		}
+		return categoria;
+	}
 
-@Transactional 
-private String getCategoriaById(String idCategoria) throws CustomException {
-String categoria = null;
-try {
-categoria = motivoCategoriaDAO.getCategoriaById(idCategoria);
-} catch (CustomException e) {
-throw e;
-} catch (Exception e) {
-throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
-}
-return categoria;
-}
+	private Contacto crearContacto(ContactoForm contactoForm, String idMotivo, String idCategoria, String correo,
+			String nroTramite, String tipo) {
+		Contacto contacto = new Contacto();
 
-private Contacto crearContacto(ContactoForm contactoForm, String idMotivo, String idCategoria, String correo, String nroTramite, String tipo) {
-Contacto contacto = new Contacto();
+		contacto.setIdtramite(nroTramite);
+		contacto.setIdAfiliado(contactoForm.getIdAfiliado());
+		contacto.setIdMotivo(Integer.valueOf(idMotivo));
+		if (idCategoria != null && !idCategoria.equals(""))
+			contacto.setIdCategoria(Integer.valueOf(idCategoria));
+		contacto.setNombreCompleto(contactoForm.getNombreAfiliado().toUpperCase());
+		contacto.setComentario(contactoForm.getComentario());
+		contacto.setCorreo(correo);
+		contacto.setTipo(tipo);
 
-contacto.setIdtramite(nroTramite);
-contacto.setIdAfiliado(contactoForm.getIdAfiliado());
-contacto.setIdMotivo(Integer.valueOf(idMotivo));
-if(idCategoria != null && !idCategoria.equals(""))
-contacto.setIdCategoria(Integer.valueOf(idCategoria));
-contacto.setNombreCompleto(contactoForm.getNombreAfiliado().toUpperCase());
-contacto.setComentario(contactoForm.getComentario());
-contacto.setCorreo(correo);
-contacto.setTipo(tipo);
+		return contacto;
+	}
 
-return contacto;
-}
+	@Transactional
+	public String getMailByIds(String idMotivo, String idCategoria) throws CustomException {
+		String correo = null;
+		try {
+			correo = motivoCategoriaDAO.getMailByIds(idMotivo, idCategoria);
+		} catch (CustomException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
+		}
+		return correo;
+	}
 
-@Transactional
-public String getMailByIds(String idMotivo, String idCategoria) throws CustomException{
-String correo = null;
-try {
-correo = motivoCategoriaDAO.getMailByIds(idMotivo,idCategoria);
-} catch (CustomException e) {
-throw e;
-} catch (Exception e) {
-throw new CustomException(e.getMessage(), ErrorMessages.UNKNOWN_ERROR);
-}
-return correo;
-}
+	private String getMessage(ContactoForm contactoForm, String motivo, String categoria) {
+		StringBuilder sb = new StringBuilder();
+		AfiliadosService restService = new AfiliadosService();
+		AfiliadosResponse restResponse = restService.getAfiliado(contactoForm.getIdAfiliado());
 
-private String getMessage(ContactoForm contactoForm, String motivo, String categoria) {
-StringBuilder sb = new StringBuilder();
-AfiliadosService restService = new AfiliadosService();
-AfiliadosResponse restResponse = restService.getAfiliado(contactoForm.getIdAfiliado());
+		sb.append(Constants.TIPO_TRAMITE).append(": ").append(Constants.CONTACTO).append("\n");
+		sb.append(Constants.MOTIVO).append(": ").append(motivo).append("\n");
+		if (categoria != null && !categoria.equals(""))
+			sb.append(Constants.CATEGORIA).append(": ").append(categoria).append("\n");
+		sb.append(Constants.TITULAR).append(": ").append(contactoForm.getIdAfiliado()).append(" - ")
+				.append(contactoForm.getNombreAfiliado().toUpperCase()).append("\n");
+		if (restResponse != null) {
+			if (restResponse.getEmail() != null && restResponse.getEmail() != "")
+				sb.append(Constants.EMAIL).append(": ").append(restResponse.getEmail()).append("\n");
+			if (restResponse.getTelefonoCasa() != null && restResponse.getTelefonoCasa() != "")
+				sb.append(Constants.TELEFONO_CASA).append(": ").append(restResponse.getTelefonoCasa()).append("\n");
+			if (restResponse.getTelefonoTrabajo() != null && restResponse.getTelefonoTrabajo() != "")
+				sb.append(Constants.TELEFONO_TRABAJO).append(": ").append(restResponse.getTelefonoTrabajo())
+						.append("\n");
+		}
 
-sb.append(Constants.TIPO_TRAMITE).append(": ").append(Constants.CONTACTO).append("\n");
-sb.append(Constants.MOTIVO).append(": ").append(motivo).append("\n");
-if(categoria != null && !categoria.equals(""))
-sb.append(Constants.CATEGORIA).append(": ").append(categoria).append("\n");
-sb.append(Constants.TITULAR).append(": ").append(contactoForm.getIdAfiliado()).append(" - ").append(contactoForm.getNombreAfiliado().toUpperCase()).append("\n");
-if(restResponse != null){
-if(restResponse.getEmail() != null && restResponse.getEmail() != "")
-sb.append(Constants.EMAIL).append(": ").append(restResponse.getEmail()).append("\n");
-if(restResponse.getTelefonoCasa() != null && restResponse.getTelefonoCasa() != "")
-sb.append(Constants.TELEFONO_CASA).append(": ").append(restResponse.getTelefonoCasa()).append("\n");
-if(restResponse.getTelefonoTrabajo() != null && restResponse.getTelefonoTrabajo() != "")
-sb.append(Constants.TELEFONO_TRABAJO).append(": ").append(restResponse.getTelefonoTrabajo()).append("\n");
-}
+		sb.append(Constants.TEXTO).append(": ").append(contactoForm.getComentario()).append("\n");
 
-sb.append(Constants.TEXTO).append(": ").append(contactoForm.getComentario()).append("\n");
+		return sb.toString();
+	}
 
-return sb.toString();
-}
+	private String getSubject(ContactoForm contactoForm, String nroTramite) {
+		StringBuilder sb = new StringBuilder();
 
-private String getSubject(ContactoForm contactoForm, String nroTramite) {
-StringBuilder sb = new StringBuilder();
+		sb.append(Constants.CONTACTO).append(" - ");
+		sb.append(nroTramite).append(" - ");
+		sb.append(contactoForm.getIdAfiliado()).append(" - ");
+		sb.append(contactoForm.getNombreAfiliado().toUpperCase());
 
-sb.append(Constants.CONTACTO).append(" - ");
-sb.append(nroTramite).append(" - ");
-sb.append(contactoForm.getIdAfiliado()).append(" - ");
-sb.append(contactoForm.getNombreAfiliado().toUpperCase());
-
-return sb.toString();
-}
-
+		return sb.toString();
+	}
 
 }
