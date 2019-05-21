97b55601642e3b7e58b1a34b41506a7ee0e810ca
Agustin <mkj155gmail.com>
2019-05-20 23:01:57 -0300

    WS autenticacion

--- OSDEPYM/src/main/java/com/osdepym/rest/AfiliadosService.java
+++ OSDEPYM/src/main/java/com/osdepym/rest/AfiliadosService.java
@@ -1,17 +1,19 @@
 package com.osdepym.rest;
 
 import java.util.Properties;
-import org.springframework.web.client.RestTemplate;
+
 import com.osdepym.rest.response.AfiliadosResponse;
 import com.osdepym.util.ReadPropertyFile;
 
 public class AfiliadosService {
+	
+	public AfiliadosService(){};
 
 	public AfiliadosResponse getAfiliado(String idAfiliado) {
 		AfiliadosResponse response = new AfiliadosResponse();
 		try{
-			RestTemplate restTemplate = new RestTemplate();
 			Properties parameters = ReadPropertyFile.readFile("WSConfiguration.properties");
+			BasicAuthRestTemplate restTemplate = new BasicAuthRestTemplate(parameters.getProperty("afiliado.user"), parameters.getProperty("afiliado.password"));
 			response = restTemplate.getForObject(parameters.getProperty("afiliado.endpoint") + idAfiliado,
 					AfiliadosResponse.class);
 		}catch(Exception e) {
