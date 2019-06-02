085db1c3a82fb1c2270623878cfda06db8879844
Agustin <mkj155gmail.com>
2019-06-02 13:15:08 -0300

    rest proyect

---
+++ api-modelopersona-rest/src/main/java/com/osdepym/rest/BasicAuthInterceptor.java
@@ -0,0 +1,34 @@
+package com.osdepym.rest;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+public class BasicAuthInterceptor implements  ClientHttpRequestInterceptor{
+
+    private String username;
+    private String password;
+
+    public BasicAuthInterceptor(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest httpRequest, byte[] bytes, ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
+        HttpHeaders headers = httpRequest.getHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, encodeCredentialsForBasicAuth(username, password));
+
+        return clientHttpRequestExecution.execute(httpRequest, bytes);
+    }
+
+    public static String encodeCredentialsForBasicAuth(String username, String password) {
+		return "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
+    }
+
+}
---
+++ api-modelopersona-rest/src/main/java/com/osdepym/rest/server/BeneficiarioServiceREST.java
@@ -0,0 +1,36 @@
+package com.osdepym.rest.server;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+
+@RestController
+public class BeneficiarioServiceREST extends RestTemplate{
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	
+	@RequestMapping(value="/getBeneficiarios", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public void getBeneficiarios(@RequestParam(value="idAfiliado") Long idAfiliado)  {
+		
+	}
+
+}
